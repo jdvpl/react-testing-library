b906f4b67cc52557190522895e7ba723c34d3e0f
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-08-29 15:53:31 -0500

    importing jest, expect function, testing if there is a tesct in the document

--- src/__tests__/Formulario.test.js
+++ src/__tests__/Formulario.test.js
@@ -1,1 +1,11 @@
-it("<Formulario/> cargar el formulario y revisar que todo sea correcto", () => {});
+import Formulario from "../components/Formulario";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+
+it("<Formulario/> cargar el formulario y revisar que todo sea correcto", () => {
+  // const wrapper = render(<Formulario />);
+  // wrapper.debug();
+  render(<Formulario />);
+  expect(screen.getByText("Crear Cita")).toBeInTheDocument();
+});
