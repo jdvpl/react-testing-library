e3bb61eadca46202640b5b8356b2400c98e248ab
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-09-25 12:00:27 -0500

    fireEvent spyfunction

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,1 +1,1 @@
-test("app workin", () => {});
+test("app working", () => {});
--- src/__tests__/Formulario.test.js
+++ src/__tests__/Formulario.test.js
@@ -1,19 +1,39 @@
 import Formulario from "../components/Formulario";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
+
+const crearCita = jest.fn();
 
 it("<Formulario/> cargar el formulario y revisar que todo sea correcto", () => {
   // const wrapper = render(<Formulario />);
   // wrapper.debug();
-  render(<Formulario />);
+  render(<Formulario crearCita={crearCita} />);
 
   // en toda la pantalla
   expect(screen.getByText("Crear Cita")).toBeInTheDocument();
   // heading
-  expect(screen.getByTestId("maintitle").textContent).toBe("Crear Cita");
-  expect(screen.getByTestId("maintitle").tagName).toBe("H2");
-  expect(screen.getByTestId("maintitle").tagName).not.toBe("H1");
+  const title = screen.getByTestId("maintitle");
+  expect(title.textContent).toBe("Crear Cita");
+  expect(title.tagName).toBe("H2");
+  expect(title.tagName).not.toBe("H1");
   // boton
-  expect(screen.getByTestId("btn-submit").tagName).toBe("BUTTON");
+  const button = screen.getByTestId("btn-submit");
+  expect(button.tagName).toBe("BUTTON");
+  expect(button.textContent).toBe("Agregar Cita");
+  expect(button.textContent).not.toBe("Agregar Nueva Cita");
+});
+
+it("<Formulario/> validacion de formulario", () => {
+  render(<Formulario crearCita={crearCita} />);
+
+  // click en el boton
+  const btnSubmit = screen.getByTestId("btn-submit");
+  fireEvent.click(btnSubmit);
+  // revisar por alerta
+  const alerta = screen.getByTestId("test-alerta");
+  expect(alerta.textContent).toBe("Todos los campos son obligatorios");
+  expect(alerta.tagName).toBe("P");
+  expect(alerta.tagName).not.toBe("BUTTON");
+  expect(alerta).toBeInTheDocument();
 });
