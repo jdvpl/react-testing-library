2c7d7f221c6d6157df7976a6303c52df4723d2fb
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-09-25 16:01:39 -0500

    pruebas de integracion

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,1 +1,17 @@
-test("app working", () => {});
+import { render, screen, fireEvent } from "@testing-library/react";
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
+import App from "../App";
+
+test("<App/> pruebas de integracion", () => {
+  // const wrapper = render(<App />);
+  // // verificar que se este mondanto el component
+  // wrapper.debug();
+  render(<App />);
+  const mainTitle = screen.getByTestId("maintitle-h1");
+  expect(mainTitle).toBeInTheDocument();
+  expect(mainTitle.textContent).toBe("Administrador de Pacientes");
+  expect(screen.getByText("No hay citas")).toBeInTheDocument();
+  expect(screen.getByText("Crear Cita")).toBeInTheDocument();
+});
